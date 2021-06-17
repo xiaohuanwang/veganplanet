901837f5a1dba13b27df814ce4504f5798425631
王小欢 <wxh_work@163.com>
2021-06-17 14:32:40 +0800

    init

---
+++ src/main/java/com/veganplanet/VeganplanetApplication.java
@@ -0,0 +1,13 @@
+package com.veganplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeganplanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeganplanetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veganplanet/VeganplanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veganplanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeganplanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
