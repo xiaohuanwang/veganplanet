38d240259b882beeae1634440870666d84e99b8d
王小欢 <wxh_work@163.com>
2021-06-18 16:35:36 +0800

    commit--init

--- src/main/java/com/veganplanet/VeganplanetApplication.java
+++ src/main/java/com/veganplanet/VeganplanetApplication.java
@@ -2,7 +2,12 @@ package com.veganplanet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @description:
+ * @author wangxiaohuan
+ * @Date 2:50 下午 2021/6/17
+   No such property: code for class: Script1
+ */
 @SpringBootApplication
 public class VeganplanetApplication {
 
